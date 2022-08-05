6b0f29913041e18cfdb1a6058e10b3470fb51d37
dbuendia <buendia.barcelo@gmail.com>
2022-08-05 16:01:29 +0200

    CSS select improvements

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -23,35 +23,48 @@ function Filters({
 
   return (
     <div className="filters">
-      <b>Filtros</b>
-      <span>País:</span>
-      <select
-        name="Country"
-        onChange={handleCountrySelectChange}
-        value={country}
-      >
-        <option value={countries.argentina}>{countries.argentina}</option>
-        <option value={countries.brasil}>{countries.brasil}</option>
-        <option value={countries.chile}>{countries.chile}</option>
-        <option value={countries.uruguay}>{countries.uruguay}</option>
-      </select>
-      <span>Precio:</span>
-      <select name="Price" onChange={handlePriceSelectChange} value={price}>
-        <option value={prices.economico}>{prices.economico}</option>
-        <option value={prices.estandar}>{prices.estandar}</option>
-        <option value={prices.comodo}>{prices.comodo}</option>
-        <option value={prices.lujoso}>{prices.lujoso}</option>
-      </select>
-      <span>Tamaño:</span>
-      <select
-        name="Capacity"
-        onChange={handleCapacitySelectChange}
-        value={capacity}
-      >
-        <option value={capacities.pequeno}>{capacities.pequeno}</option>
-        <option value={capacities.mediano}>{capacities.mediano}</option>
-        <option value={capacities.grande}>{capacities.grande}</option>
-      </select>
+      <p className="filter-title">Filtros</p>
+      <div>
+        <span>País:</span>
+        <select
+          name="Country"
+          onChange={handleCountrySelectChange}
+          value={country}
+          className="minimal"
+        >
+          <option value={countries.argentina}>{countries.argentina}</option>
+          <option value={countries.brasil}>{countries.brasil}</option>
+          <option value={countries.chile}>{countries.chile}</option>
+          <option value={countries.uruguay}>{countries.uruguay}</option>
+        </select>
+      </div>
+      <div>
+        <span>Precio:</span>
+        <select
+          name="Price"
+          onChange={handlePriceSelectChange}
+          value={price}
+          className="minimal"
+        >
+          <option value={prices.economico}>{prices.economico}</option>
+          <option value={prices.estandar}>{prices.estandar}</option>
+          <option value={prices.comodo}>{prices.comodo}</option>
+          <option value={prices.lujoso}>{prices.lujoso}</option>
+        </select>
+      </div>
+      <div>
+        <span>Tamaño:</span>
+        <select
+          name="Capacity"
+          onChange={handleCapacitySelectChange}
+          value={capacity}
+          className="minimal"
+        >
+          <option value={capacities.pequeno}>{capacities.pequeno}</option>
+          <option value={capacities.mediano}>{capacities.mediano}</option>
+          <option value={capacities.grande}>{capacities.grande}</option>
+        </select>
+      </div>
     </div>
   );
 }
